5263309681b126fa6ae33aefb58064e0ebbc84be
astralamy <amy.lambert@coda-student.school>
2025-10-17 11:34:01 +0000

    add user input into board

--- functions/tic_tac_toe.c
+++ functions/tic_tac_toe.c
@@ -7,11 +7,21 @@
 void tic_tac_toe()
 {
     char **boardValues = malloc(9 * sizeof(*boardValues)); // allocation of memory for board creation
-    boardValues[8] = "\0";                                 // makes sure there is an end to board
-    for (int i = 0; i < 9; i++)                            // initialises the values to empty spaces
+    boardValues[8] = "\0";
+    int counter = 0;                          // makes sure there is an end to board
+    for (counter = 0; counter < 9; counter++) // initialises the values to empty spaces
     {
-        boardValues[i] = " ";
+        boardValues[counter] = " ";
     }
-    print_tab(boardValues);
+    counter = 0;
+    int choice = 0;
+    while (counter < 9)
+    {
+        printf("Choisissez où mettre votre symbole (de 0 à 8 en partant du coin haut gauche):");
+        scanf("%d", &choice);
+        boardValues[choice] = "X";
+        print_tab(boardValues);
+        counter++;
+    }
     free(boardValues);
 }
---
+++ functions/win_check.c
@@ -0,0 +1,5 @@
+// insert imports here
+#include <stdlib.h>
+#include <stdio.h>
+
+// main script
